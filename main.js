9dc6402be0142ebd6d25dcf33d46c24534cda2b1
Luca Ravezzani <liuca@rocketmail.com>
2019-11-20 12:52:47 +0100

    base64UrlToUint8Array

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function subscribe() {
     navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
       serviceWorkerRegistration.pushManager.subscribe({
           userVisibleOnly: true,
-          applicationServerKey: "AAAA5IDnmoI:APA91bFlarD-p3K_IkezqaXgQNKdvEq5Im6b1fjzg-N4JCDLYV87d49adigrEB6Onl8Bs2NBbz8BUPe1tfQ2CSiFCty_LrScFgwJPpAGQ63GDYjloleXuWu4nawMLLSHdGryBUZy7qns"
+          applicationServerKey: window.base64UrlToUint8Array("AAAA5IDnmoI:APA91bFlarD-p3K_IkezqaXgQNKdvEq5Im6b1fjzg-N4JCDLYV87d49adigrEB6Onl8Bs2NBbz8BUPe1tfQ2CSiFCty_LrScFgwJPpAGQ63GDYjloleXuWu4nawMLLSHdGryBUZy7qns")
         })
         .then(function(subscription) {
           // The subscription was successful
